ddc4b543b5a705e4f192b7ca2bc9dc1ec1c62556
Mateusz Skop <skopekv21@gmail.com>
2023-03-28 20:36:45 +0200

    I co? Działa?

---
+++ RTS Multiplayer/Assets/Scenes/DELETE_THIS.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DELETE_THIS : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // https://www.youtube.com/watch?v=dQw4w9WgXcQ
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
