458f390306e48e8cf2456da157d289bcd1134828
Mateusz Skop <skopekv21@gmail.com>
2023-05-21 17:11:29 +0200

    FIGHT

--- RTS Multiplayer/Assets/Fight.cs
+++ RTS Multiplayer/Assets/Fight.cs
@@ -1,18 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Fight : MonoBehaviour
 {
+    public Unit stats;
+
+    private float range;
+    private float reloadTime;
+    private float accuracy;
+    private float damage;
+
+    private float reloadProgres = 0;
+    public GameObject target;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        range = stats.attackRange;
+        reloadTime = 60f / stats.attackSpeed;
+        accuracy = stats.accuracity;
+        damage = stats.attackDamage;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (reloadProgres < reloadTime)
+        {
+            reloadProgres += Time.deltaTime;
+            return;
+        }
+
+        if (target == null)
+        {
+            target = gameObject.GetComponent<AiTargetingSystem>().target;
+            return;
+        }
+
+        if (range > Vector3.Magnitude(target.transform.position - transform.position))
+        {
+            return;
+        }
+
+
+    }
+
+    private void Shot(GameObject target)
+    {
         
+    }
+
+    private void CalculateHit(GameObject target)
+    {
+        float distance
     }
 }
